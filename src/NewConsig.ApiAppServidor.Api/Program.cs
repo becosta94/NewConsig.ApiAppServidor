7a4c19599036af5337831f0a166ca140804b15a6
Bernardo Costa <bernardo@consultoriabsafe.com.br>
2025-08-27 17:34:38 -0300

    Correção Program

--- src/NewConsig.ApiAppServidor.Api/Program.cs
+++ src/NewConsig.ApiAppServidor.Api/Program.cs
@@ -69,11 +69,11 @@ builder.Services.AddSwaggerGen(c =>
 });
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseAuthentication();
 
